64d8b003ab432ab0dc7682560b5d5563b38c631b
Necho <2839868773@qq.com>
2023-11-01 05:27:28 +0800

    配置七牛云

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class User(BaseModel):
+    id: int
+    name: str
 
 
 @app.get("/")
@@ -8,6 +14,11 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/hello/{user_id}")
+async def say_hello(user_id: int, user: User):
+    return {
+        "message": f"Hello {user.name}",
+        "id": user.id,
+        "name": user.name,
+        "user_id": user_id
+    }
---
+++ store.py
@@ -0,0 +1,19 @@
+from qiniu import Auth
+import qiniu.config
+
+Access_Key = 'Access_Key'
+Secret_Key = 'Secret_Key'
+
+# 构建鉴权对象
+query = Auth(Access_Key, Secret_Key)
+
+# 确定上传的空间
+Bucket_Name = 'Bucket_Name'
+
+# 准备保存的文件名
+File_Name = 'File_Name'
+
+# 视频自动化处理队列
+PipeLine = 'PipeLine'
+
+
